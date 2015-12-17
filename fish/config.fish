a60eefef2044807314d9012ba6630d06d078c3ae
Rik Jurriaans <rjurriaans@elmar.nl>
2015-12-17 15:12:01 +0100

    added fish theme

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+. ~/.config/fish/aliases.fish
+
+# vi mode, start in insert mode
+set fish_bind_mode insert
+
+set -g theme_display_git yes
+set -g theme_display_git_untracked no
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+set -g theme_display_ruby no
+set -g theme_display_user no
+set -g theme_display_vi yes
+set -g theme_display_vi_hide_mode default
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g default_user rikj
+
+set -g theme_display_vi yes
