3987e6571a98ddec21770e5ebc11a330090b6a1c
Rik Jurriaans <rjurriaans@elmar.nl>
2015-12-18 17:22:30 +0100

    WIP fish theme bases upon gruvbox

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -5,5 +5,5 @@ function fish_right_prompt -d 'bobthefish is all about the right prompt'
   set_color $fish_color_autosuggestion[1]
   set -q theme_date_format; or set -l theme_date_format "+%c"
   date $theme_date_format
-  set_color normal
+  set_color 333
 end
