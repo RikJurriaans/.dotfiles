a15cce62a09acc259efc724941e68ada794a43ae
Rik Jurriaans <rjurriaans@elmar.nl>
2015-12-22 17:18:05 +0100

    WIP pimping the terminal by looking where I am so I can configure stuff cooler

---
+++ fish/env.fish
@@ -0,0 +1,9 @@
+# set default PATH
+
+set -g USER (whoami)
+
+if test $USER = "rikjur"
+  set -g ELMAR true
+end
+
+set -gx PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /home/rikjur/.rbenv/shims
