eda163f0f648928a7715acf2421149fee35cf541
RikJurriaans <rik@jurriaans.org>
2015-12-23 21:26:10 +0100

    Deleted unused files, added terminal theme from home

--- fish/env.fish
+++ fish/env.fish
@@ -1,10 +1,11 @@
-# set default PATH
-
 set -g USER (whoami)
 
 if test $USER = "rikjur"
   set -g ELMAR true
+else test $USER = "rik"
+  set -g LAPTOP true
 end
 
+# set default PATH
 set -gx PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /home/rikjur/.rbenv/shims /usr/local/go/bin
 
