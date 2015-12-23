721258394f80d567f5f135ad3c68574c1e3caea3
Rik Jurriaans <rjurriaans@elmar.nl>
2015-12-23 09:52:19 +0100

    WIP make tmux startup at opening of terminal.

--- fish/config.fish
+++ fish/config.fish
@@ -14,8 +14,13 @@ if test (which rbenv 2>/dev/null)
   status --is-interactive; and . (rbenv init -|psub)
 end
 
+tmux_attach
+or tmux_new_session
+or echo "tmux failed to start; using plain fish shell"
+
 set -g fish_key_bindings fish_vi_key_bindings
 
 set fish_bind_mode insert
 
 set -U EDITOR emacs
+
