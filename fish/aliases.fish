a771adc8051ee650a3e405043a3d46b15d506c64
Rik Sanchez <rjurriaans@elmar.nl>
2016-11-09 16:50:10 +0100

    Added VI mode the my tmux config once again.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,3 +3,5 @@ alias kj "killall java & killall tail"
 alias emax "emacsclient -t"
 alias semac "sudo emacsclient -c -a emacs"
 alias emacsc "emacsclient -c -a emacs"
+
+alias clearcache-local-vd "http -f POST localhost:3000/clj/cache/clear key=2Hgroi889"
